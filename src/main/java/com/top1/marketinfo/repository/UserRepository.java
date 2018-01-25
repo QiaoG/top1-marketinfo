2a54c0a5f76bffb2757d6adfb73c7b49852c48ca
高俏 <gaoqiao@gaoqiaodeMacBook-Pro.local>
2018-01-25 21:36:35 +0800

    mofify

--- src/main/java/com/top1/marketinfo/repository/UserRepository.java
+++ src/main/java/com/top1/marketinfo/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package com.top1.marketinfo.repository;
 
+import com.top1.marketinfo.entity.Discuss;
 import com.top1.marketinfo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.List;
 
@@ -11,8 +15,10 @@ import java.util.List;
 * Date:2018/1/7
 * Time:上午8:35
 */
-//@RepositoryRestResource(path="user")
+@RepositoryRestResource(path="user")
 public interface UserRepository extends JpaRepository<User,Long> {
-    @Override
-    List<User> findAll();
+    @RestResource(path = "findByNickname",rel = "findByNickname")
+    //@Query(value = "select * from user where nickname=? ",nativeQuery = true)
+    List<User> findByNickname(@Param("name")String name);
+
 }
